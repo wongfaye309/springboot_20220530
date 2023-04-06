2c0b608414023960020648286abdc69bc5a2d2ee
yiht7 <yiht7@chinaunicom.cn>
2023-04-06 16:01:52 +0800

    test1

--- src/main/java/com/example/springboot_20220530/controller/HelloController.java
+++ src/main/java/com/example/springboot_20220530/controller/HelloController.java
@@ -5,10 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
-@Controller
+@RestController
 public class HelloController {
     @Resource
     private Person person;
